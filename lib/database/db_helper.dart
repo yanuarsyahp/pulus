587a69bbb36129bbe3e590649fd86a0443ba3489
Yanuarsyah Pratama <yanuarsyahpratama@gmail.com>
2024-01-27 00:53:35 +0700

    cart_provider

---
+++ lib/database/db_helper.dart
@@ -0,0 +1,35 @@
+import 'package:pulus/features/shop/model/cart_model.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+import 'package:collection/collection.dart';
+import 'dart:io' as io;
+
+class DBHelper {
+  static Database? _db;
+
+  Future<Database?> get db async {
+    if (_db != null) {
+      return _db!;
+    }
+    _db = await initDatabase();
+  }
+
+  initDatabase() async {
+    io.Directory documentaryDirectory =
+        await getApplicationDocumentsDirectory();
+    String path = join(documentaryDirectory.path, 'cart.db');
+    var db = await openDatabase(path, version: 1, onCreate: _onCreate);
+  }
+
+  _onCreate(Database db, int version) async {
+    await db.execute(
+        'CREATE TABLE cart (id INTEGER PRIMARY KEY , productId VARCHAR UNIQUE,productName TEXT,initialPrice INTEGER, productPrice INTEGER , quantity INTEGER, unitTag TEXT , image TEXT )');
+  }
+
+  Future<Cart> insert(Cart cart) async {
+    var dbClient = await db;
+    await dbClient!.insert('cart', cart.toMap());
+    return cart;
+  }
+}
